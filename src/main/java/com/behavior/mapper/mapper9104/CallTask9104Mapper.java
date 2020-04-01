00b51eb9b16a947975109df048e9b649258c3ab3
cobin <cobin@21cn.com>
2020-04-01 13:53:57 +0800

    OK

--- src/main/java/com/behavior/mapper/mapper9104/CallTask9104Mapper.java
+++ src/main/java/com/behavior/mapper/mapper9104/CallTask9104Mapper.java
@@ -5,10 +5,48 @@ import org.apache.ibatis.annotations.Param;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * 财务流水记录进行整合处理，形成详细的流水记录流
+ * @author  Cobin
+ * @date    2020/4/1 9:41
+ * @version 1.0
+*/
 public interface CallTask9104Mapper {
+	/**
+	 * 调用财务更新过程，对新数据进行增加和修改的数据更新，无效的数据进行删除
+	 * @param update 条件参数
+	 * @return
+	 */
 	int updateFinMerDataSystemToday(Map<Object, Object> update);
+
+	/**
+	 * 更新财务流水数据
+	 * @param traceList 记录集合
+	 * @return
+	 */
 	String updateFinMerDataSystemFlowDetails(@Param("traceList") List<List<Map<String,Integer>>> traceList);
+
+	/**
+	 * 获取需要处理的资金流水号
+	 * @return
+	 */
 	List<Map<String,Integer>> getFinMerDataSystemTodayIds();
+
+	/**
+	 * 获取所有的资金流水信息
+	 * @return
+	 */
 	List<Map<String,Object>> getFinMerDataSystemAll();
+
+	/**
+	 * 获取已经存在的资金流水父节点的记录
+	 * @return
+	 */
 	List<Map<String,Object>> getFinMerDataSystemFlowDetail();
+
+	/**
+	 * 获取已经存在的资金流水子节点分支的记录
+	 * @return
+	 */
+	List<Map<String,Object>> getFinMerDataSystemFlowDetailBranch();
 }
--- src/main/java/com/behavior/scheduler/WorkCallFinMerDataSystemNotify.java
+++ src/main/java/com/behavior/scheduler/WorkCallFinMerDataSystemNotify.java
@@ -1,7 +1,6 @@
 package com.behavior.scheduler;
 
 import com.behavior.BehaviorMain;
-import com.behavior.mapper.mapper9103.CallTask9103Mapper;
 import com.behavior.mapper.mapper9104.CallTask9104Mapper;
 import com.cobin.util.Tools;
 import org.quartz.JobExecutionContext;
@@ -33,20 +32,35 @@ public class WorkCallFinMerDataSystemNotify extends WorkJob {
 	public void execWork(BehaviorMain bm,String sdate ){
 		long curTime = System.currentTimeMillis();
 		CallTask9104Mapper ct9104 = bm.getMapper(CallTask9104Mapper.class);
+		//执行本日数据处理
+		Map<Object,Object> param = new HashMap<>(5);
+		param.put("dataType",1);
+		ct9104.updateFinMerDataSystemToday(param);
+		log.info("财务处理结果,"+param);
 		//所有的ID集合
 		List<Map<String,Integer>> ids = ct9104.getFinMerDataSystemTodayIds();
-		//所有的财务数据
-		Map<Integer,Map<Integer,Map<String,Object>>> query = getFinMerAll(ct9104);
 		//所有的已经存在的流水记录
 		Map<String,Map<String,Object>> exitFlowDetail = getFowDetails(ct9104);
-		List<Map<String,Integer>> matchFlows = new ArrayList<>();
-		Map<String,String> matchExist = new HashMap<>();
+		Map<String,Map<String,Object>> exitFlowDetailBranch = getFowDetailsBranch(ct9104);
+		//所有的财务数据
+        List<Map<String,Object>> resultFinMer = ct9104.getFinMerDataSystemAll();
+		Map<Integer,Map<Integer,Map<String,Object>>> query = getFinMerAll(resultFinMer,false);
+		Map<Integer,Map<Integer,Map<String,Object>>> queryBranch = getFinMerAll(resultFinMer,true);
+		List<Map<String,Integer>> matchFlows = new ArrayList<>(resultFinMer.size()*3);
+		Map<String,String> matchExist = new HashMap<>(resultFinMer.size()*2);
 		//遍历访问此流水号是否有父类
 		for(Map<String,Integer> id:ids){
 			int rId = id.get("rId");
-			searchFinMerId(matchFlows, matchExist, exitFlowDetail, query, rId, new int[]{rId});
+			searchFinMerId(matchFlows, matchExist, exitFlowDetail, query, rId, new int[]{rId},false);
 		}
-		log.info("财务流水处理,耗时:"+(System.currentTimeMillis()-curTime)+">>>>总数："+matchExist.size()+"("+matchFlows.size()+")");
+		matchExist.clear();
+		//遍历访问此流水号是否有父类
+		for(Map<String,Integer> id:ids){
+			int rId = id.get("rId");
+			searchFinMerId(matchFlows, matchExist, exitFlowDetailBranch, queryBranch, rId, new int[]{rId},true);
+		}
+
+		log.info("财务流水处理,耗时:"+(System.currentTimeMillis()-curTime)+">>>>总数："+matchFlows.size()+"("+matchFlows.size()+")");
 		curTime = System.currentTimeMillis();
 		List<List<Map<String,Integer>>> resultFlow = new ArrayList<>();
 		List<Map<String,Integer>> rowFlow = null ;
@@ -79,7 +93,7 @@ public class WorkCallFinMerDataSystemNotify extends WorkJob {
 	 * @param rId			需要查询的ID
 	 * @param curRids		当前ID属于的父类ID集合
 	 */
-	private void searchFinMerId(List<Map<String,Integer>> matchFlows ,Map<String,String> matchExist,Map<String,Map<String,Object>> exitFlowDetail,Map<Integer,Map<Integer,Map<String,Object>>> query,int rId,int[] curRids){
+	private void searchFinMerId(List<Map<String,Integer>> matchFlows ,Map<String,String> matchExist,Map<String,Map<String,Object>> exitFlowDetail,Map<Integer,Map<Integer,Map<String,Object>>> query,int rId,int[] curRids,boolean isBranch){
 		Map<Integer,Map<String,Object>> row = query.get(rId);
 		for(Map.Entry<Integer,Map<String,Object>> me:row.entrySet()){
 			int _mId = me.getKey();
@@ -95,14 +109,14 @@ public class WorkCallFinMerDataSystemNotify extends WorkJob {
 				int[] _curRids = new int[curRids.length+1];
 				int i = 0;
 				for(int curRid:curRids) {
-					addMatchFlow(matchFlows, matchExist,exitFlowDetail,mv, curRid, _mId);
+					addMatchFlow(matchFlows, matchExist,exitFlowDetail,mv, curRid, _mId,isBranch?2:1);
 					_curRids[i] = curRid;
 					i++;
 				}
 				_curRids[i] = _mId;
-				searchFinMerId(matchFlows,matchExist,exitFlowDetail,query,_mId,_curRids);
+				searchFinMerId(matchFlows,matchExist,exitFlowDetail,query,_mId,_curRids,isBranch);
 			}else {
-				addMatchFlow(matchFlows, matchExist,exitFlowDetail,mv, rId, _mId);
+				addMatchFlow(matchFlows, matchExist,exitFlowDetail,mv, rId, _mId,isBranch?2:1);
 			}
 		}
 	}
@@ -116,7 +130,7 @@ public class WorkCallFinMerDataSystemNotify extends WorkJob {
 	 * @param rId
 	 * @param _mId
 	 */
-	private void addMatchFlow(List<Map<String, Integer>> matchFlows, Map<String, String> matchExist,Map<String,Map<String,Object>> exitFlowDetail,Map<String,Object> curRowData, int rId, int _mId) {
+	private void addMatchFlow(List<Map<String, Integer>> matchFlows, Map<String, String> matchExist,Map<String,Map<String,Object>> exitFlowDetail,Map<String,Object> curRowData, int rId, int _mId,int branch) {
 		String _key = rId+"_"+_mId;
 		if(!matchExist.containsKey(_key)) {
 			boolean isOk = true;
@@ -132,6 +146,7 @@ public class WorkCallFinMerDataSystemNotify extends WorkJob {
 				Map<String, Integer> mv = new HashMap<>(2);
 				mv.put("rId", rId);
 				mv.put("flowRid", _mId);
+				mv.put("branch",branch);
 				matchFlows.add(mv);
 			}
 			matchExist.put(_key,"");
@@ -140,11 +155,10 @@ public class WorkCallFinMerDataSystemNotify extends WorkJob {
 
 	/**
 	 * 得到所有的财务数据
-	 * @param ct9104
+	 * @param result
 	 * @return
 	 */
-	private Map<Integer,Map<Integer,Map<String,Object>>> getFinMerAll(CallTask9104Mapper ct9104){
-		List<Map<String,Object>> result = ct9104.getFinMerDataSystemAll();
+	private Map<Integer,Map<Integer,Map<String,Object>>> getFinMerAll(List<Map<String,Object>> result,boolean isBranch){
 		Map<Integer,Map<Integer,Map<String,Object>>> vals = new HashMap<>(result.size());
 		Map<Integer,Map<String,Object>> mapResult = new HashMap<>(result.size());
 		for(Map<String ,Object> r:result){
@@ -158,12 +172,20 @@ public class WorkCallFinMerDataSystemNotify extends WorkJob {
 			Integer gId = (Integer)r.get("groupRelationId");
 			if(gId!=null){
 				//合并而来，本资金号作为合并后的资金号的父类
-				addFinMerId(vals,gId,r);
+				if(isBranch){
+					addFinMerId(vals,rId,mapResult.get(gId));
+				}else {
+					addFinMerId(vals, gId, r);
+				}
 			}
 			Integer sId = (Integer)r.get("splitRelationId");
 			if(sId!=null){
 				//拆分而来，拆分的资金号作为本资金号的父类
-				addFinMerId(vals,rId,mapResult.get(sId));
+				if(isBranch){
+					addFinMerId(vals,sId,r);
+				}else {
+					addFinMerId(vals, rId, mapResult.get(sId));
+				}
 			}
 			String _claim = (String) r.get("claim");
 			if (_claim.length() > 8) {
@@ -173,7 +195,11 @@ public class WorkCallFinMerDataSystemNotify extends WorkJob {
 					int _claimId = Tools.getInt(v[v.length-1].trim());
 					if(_claimId>0) {
 						//退货而来的新资金号，本资金号作为新资金号的父类
-						addFinMerId(vals, _claimId, r);
+						if(isBranch){
+							addFinMerId(vals, rId, mapResult.get(_claimId));
+						}else {
+							addFinMerId(vals, _claimId, r);
+						}
 						r.put("newRelationId", _claimId);
 					}
 					v = Tools.regExPickUp(_claim, "原始资金号[^\\d]{0,10}([\\d]+)");
@@ -181,7 +207,11 @@ public class WorkCallFinMerDataSystemNotify extends WorkJob {
 						_claimId = Tools.getInt(v[v.length - 1].trim());
 						if (_claimId > 0) {
 							//退货而来的原始资金号，原始资金号作为本资金号的父类
-							addFinMerId(vals, rId, mapResult.get(_claimId));
+							if(isBranch){
+								addFinMerId(vals,_claimId, mapResult.get( rId));
+							}else {
+								addFinMerId(vals, rId, mapResult.get(_claimId));
+							}
 						}
 					}
 				}else{
@@ -191,7 +221,11 @@ public class WorkCallFinMerDataSystemNotify extends WorkJob {
 						int _claimId = Tools.getInt(v[v.length - 1].trim());
 						if (_claimId > 0) {
 							//退款而来的原始资金号，本资金号作为原始资金号的父类
-							addFinMerId(vals, _claimId, r);
+							if(isBranch){
+								addFinMerId(vals, rId, mapResult.get(_claimId));
+							}else {
+								addFinMerId(vals, _claimId, r);
+							}
 						}
 					}
 				}
@@ -201,18 +235,38 @@ public class WorkCallFinMerDataSystemNotify extends WorkJob {
 	}
 
 	/**
-	 * 把存在的流水记录进行Map化，便于查询
+	 * 获取资金流水的父节点集合
 	 * @param ct9104
 	 * @return
 	 */
 	private Map<String,Map<String,Object>> getFowDetails(CallTask9104Mapper ct9104){
 		List<Map<String,Object>> result = ct9104.getFinMerDataSystemFlowDetail();
+		return getFowDetails(result);
+	}
+
+	/**
+	 * 获取资金流水的子节点集合
+	 * @param ct9104
+	 * @return
+	 */
+	private Map<String,Map<String,Object>> getFowDetailsBranch(CallTask9104Mapper ct9104){
+		List<Map<String,Object>> result = ct9104.getFinMerDataSystemFlowDetailBranch();
+		return getFowDetails(result);
+	}
+
+	/**
+	 * List记录集合转为Map集合
+	 * @param result
+	 * @return
+	 */
+	private Map<String,Map<String,Object>> getFowDetails(List<Map<String,Object>> result){
 		Map<String,Map<String,Object>> flows = new HashMap<>(result.size());
 		for(Map<String,Object> row:result){
 			flows.put(row.get("rId")+"_"+row.get("flowRid"),row);
 		}
 		return flows;
 	}
+
 
 	/**
 	 * 每条记录有N条相关的记录列表
